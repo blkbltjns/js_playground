86fec2d46c7bebca3e0c1981eef40293a79a0815
Mike Gates <mgates@b3insight.com>
2019-11-27 10:59:35 -0700

    changed customelementbase logic to use style element at root

--- InnerBottomLeftElement.js
+++ InnerBottomLeftElement.js
@@ -3,6 +3,5 @@ import CustomElementBase from "./CustomElementBase.js";
 export default class InnerBottomLeftElement extends CustomElementBase {
     onConnected(rootElement) {
         rootElement.innerHTML = `InnerBottomLeftElement`;
-        return './MikesInnerElementOne.css';
     }
 }
