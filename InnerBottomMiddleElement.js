013b98646e5e53e13a4517d2c82cbb546d30139f
Mike Gates <mgates@b3insight.com>
2019-11-27 18:12:10 -0700

    a

---
+++ InnerBottomMiddleElement.js
@@ -0,0 +1,26 @@
+import CustomElementBase from "./CustomElementBase.js";
+
+export default class InnerBottomMiddleElement extends CustomElementBase {
+    onConnected(rootElement) {
+        let table = document.createElement('div');
+        table.style.gridAutoRows = 'minmax(min-content, min-content);';
+        table.style.height = '0';
+        table.style.gridAutoFlow = 'row';
+
+        for (let i = 0; i < 30; i++) {
+            let row = document.createElement('div');
+            row.className = 'row';
+            row.innerText = `row number ${i}`;
+            table.append(row);
+        }
+
+        rootElement.append(table);
+
+        return `
+            :host {
+                background: orange;                
+                grid-auto-rows: minmax(min-content, min-content);                
+            }
+        `
+    }
+}
